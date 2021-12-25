caacabcbe5b9a325b5c8c9a535e753024543e35d
Tobias Schröder <tbs.schroeder>
2021-12-25 10:37:43 +0100

    Add day 01 of 2021

---
+++ 2021/README.md
@@ -0,0 +1,29 @@
+# Advent of Code 2020
+
+This repository contains solutions for [Advent of Code 2021](https://adventofcode.com/2021/) written in Python 3.
+
+* Day 01: ✓
+* Day 02: x
+* Day 03: x
+* Day 04: x
+* Day 05: x
+* Day 06: x
+* Day 07: x
+* Day 08: x
+* Day 09: x
+* Day 10: x
+* Day 11: x
+* Day 12: x
+* Day 13: x
+* Day 14: x
+* Day 15: x
+* Day 16: x
+* Day 17: x
+* Day 18: x
+* Day 19: x
+* Day 20: x
+* Day 21: x
+* Day 22: x
+* Day 23: x
+* Day 24: x
+* Day 25: x
---
+++ 2021/day01.py
@@ -0,0 +1,23 @@
+from lib import get_input
+
+
+def part1():
+    data = get_input('./input/input01.txt', '\n')
+    no_data = [int(line) for line in data]
+    depth = [no_data[i - 1] < line for i, line in enumerate(no_data)]
+    print(sum(depth))
+
+
+def part2():
+    data = get_input('./input/input01.txt', '\n')
+    no_data = [int(line) for line in data]
+    windows = [sum(x) for x in zip(no_data, no_data[1:], no_data[2:])]
+    pairs = zip(windows, windows[1:])
+    depth = [y - x for x, y in pairs if y - x > 0]
+    print(len(depth))
+    return
+
+
+if __name__ == "__main__":
+    part1()
+    part2()
